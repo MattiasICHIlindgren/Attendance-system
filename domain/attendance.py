f8916992e5852a607d0360fd7d0d96795028a18d
Mattias Lindgren <mattias.lindgren@svab.se>
2026-02-02 22:03:47 +0100

    early structure code for commit

---
+++ domain/attendance.py
@@ -0,0 +1,12 @@
+class Attendance: 
+    def __init__(self):
+        pass
+    
+    def mark(self,name,date):
+        pass
+    
+    def unmark(self,name,date): 
+        pass 
+    
+    def is_present(self,name,date):
+        pass 
