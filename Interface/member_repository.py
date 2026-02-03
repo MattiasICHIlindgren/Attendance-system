c50f4925227b471f1afa69e79648b0c3402ea10b
Mattias Lindgren <mattias.lindgren@svab.se>
2026-02-03 12:55:08 +0100

    added save member and imported member class from domain to meber repository. Fixed data types

--- Interface/member_repository.py
+++ Interface/member_repository.py
@@ -1,14 +1,19 @@
 from abc import ABC, abstractmethod 
+from domain.member import Member
 
 class MemberRepository(ABC): 
     @abstractmethod
-    def get_by_id(self,member_id):
+    def get_by_id(self,member_id: int) -> Member | None:
         pass
     
     @abstractmethod
-    def get_all(self):
+    def get_all(self) -> list[Member]:
         pass 
     
     @abstractmethod
-    def get_active_member_by_id(self,member_id):
+    def get_active_member_by_id(self,member_id: int) -> Member | None:
         pass
+    
+    @abstractmethod
+    def save_member(self,member: Member) -> None:
+        pass 
