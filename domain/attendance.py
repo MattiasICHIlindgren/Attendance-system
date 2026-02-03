ff412eedcfdabf8eb132f7963413229299614550
Mattias Lindgren <mattias.lindgren@svab.se>
2026-02-03 12:47:23 +0100

    corrected the Attendance init with corect datatype

--- domain/attendance.py
+++ domain/attendance.py
@@ -1,5 +1,5 @@
 class Attendance: 
-    def __init__(self,member_id,attendance_date):
+    def __init__(self,member_id: int,attendance_date):
         self.member_id = member_id 
         self.attendance_date = attendance_date
         
